8033c958ed762b249a7025740575527ec4210412
Charls Francis M. Iglesia <cfmiglesia@headoffice.corp.bpi.com.ph>
2025-12-04 16:26:35 +0800

    Add M2_Activity1

---
+++ M2_Activity1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Libro nokbuk = new Libro();
+        nokbuk.booktitle();
+
+        Libro pahina = new Libro();
+        pahina.pagenum();
+    }
+}
