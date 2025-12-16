579a8179c478ea5135505ab8f0309c9ba75767ec
Charls Francis M. Iglesia <cfmiglesia@headoffice.corp.bpi.com.ph>
2025-12-16 17:49:04 +0800

    add M3_Activity3

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -6,13 +7,13 @@ public class Main {
     static Scanner scn = new Scanner(System.in);
 
     public static void main(String[] args) {
-        HashSet<String> products = new HashSet<>();
+        HashMap<String, Integer> products = new HashMap<>();
 
-        products.add("Laptop");
-        products.add("Monitor");
-        products.add("Mouse");
-        products.add("Keyboard");
-        products.add("Printer");
+        products.put("Laptop", 30000);
+        products.put("Monitor",5000);
+        products.put("Mouse",1500);
+        products.put("Keyboard",3000);
+        products.put("Printer",6500);
 
         String choice;
         boolean isRunning = true;
@@ -22,8 +23,9 @@ public class Main {
             // MENU
             System.out.println("1. Search a product");
             System.out.println("2. Add a product");
-            System.out.println("3. Print all products and count");
-            System.out.println("4. Exit");
+            System.out.println("3. Print all products and prices");
+            System.out.println("4. Find the cheapest product");
+            System.out.println("5. Exit");
 
             // PROCESS USERS CHOICE
             System.out.print("Enter Choice: ");
@@ -34,51 +36,83 @@ public class Main {
                 case "1" -> searchProduct(products);
                 case "2" -> addProduct(products);
                 case "3" -> printProduct(products);
-                case "4" -> {
+                case "4" -> findProduct(products);
+                case "5" -> {
                     isRunning = false;
                     System.out.println("\nExiting...");
                 }
             }
         }
         scn.close();
-      }
+    }
 
 
-    public static void searchProduct(HashSet<String> productSearch) {
+    public static void searchProduct(HashMap<String, Integer> productSearch) {
 
         System.out.print("Enter product name to search: ");
         String search = scn.nextLine().trim().toLowerCase();
 
         boolean found = false;
-        for (String product : productSearch) {
-            if (product.equalsIgnoreCase(search)) {
-                System.out.println("Product found: " + product +"\n");
+
+        for (Map.Entry<String, Integer> entry : productSearch.entrySet()) {
+            String productName = entry.getKey();
+            Integer price = entry.getValue();
+
+            if (productName.equalsIgnoreCase(search)) {
+                System.out.println("Product found: " + productName + " Price: " + price + "\n");
                 found = true;
                 break;
             }
         }
+
         if (!found) {
             System.out.println("Product not found!\n");
         }
     }
 
-    public static void addProduct(HashSet<String> productAdd) {
+
+    public static void addProduct(HashMap<String, Integer> productAdd) {
 
         System.out.print("Enter product name to add: ");
         String addProduct = scn.nextLine();
 
-        if (productAdd.add(addProduct)) {
-            System.out.println("Product added: " + addProduct + "\n");
-        } else {
-            System.out.println("Product already exists.");
+        if (productAdd.containsKey(addProduct)) {
+            System.out.println("Product already exists. Current price: "
+                    + productAdd.get(addProduct) + "\n");
+            return;
         }
-    }
+        System.out.print("Enter price: ");
+        Integer addPrice = scn.nextInt();
 
-    public static void printProduct(HashSet<String> productPrint) {
+        productAdd.put(addProduct, addPrice);
+        System.out.println("Product Added: " + addProduct + "\n");
+        }
+
+        
+    public static void printProduct(HashMap<String, Integer> productPrint) {
+
         System.out.println("\nAll products:");
-        for (String product : productPrint) {
-            System.out.println(product);
+
+        for (Map.Entry<String, Integer> entry : productPrint.entrySet()) {
+            String productName = entry.getKey();
+            Integer price = entry.getValue();
+            System.out.println(productName + "-" + price);
         }
         System.out.println("Total unique products: " + productPrint.size()+ "\n");
     }
+
+    public static void findProduct(HashMap<String, Integer> productFind) {
+            String productName = null;
+            int cheapestPrice = Integer.MAX_VALUE;
+              for (Map.Entry<String, Integer> entry : productFind.entrySet()) {
+
+            if(entry.getValue() < cheapestPrice){
+                productName = entry.getKey();
+                cheapestPrice = entry.getValue();
+               }
+        }
+        System.out.println("Cheapest Product: " + productName + " - " + cheapestPrice + "\n");
+
+    }
+
 }
