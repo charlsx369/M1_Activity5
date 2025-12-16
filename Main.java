3b1bd5d9ad21ea71b771af4a264afd6500b21235
Charls Francis M. Iglesia <cfmiglesia@headoffice.corp.bpi.com.ph>
2025-12-16 17:43:22 +0800

    Add M3_Activity3

--- Main.java
+++ Main.java
@@ -1,27 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        List<String> products = new ArrayList<>();
 
-        Vehicle car = new Car(4, "Mitsubishi");
-        Vehicle truck = new Truck(16, "Scania");
+        products.add("Laptop");
+        products.add("Mouse");
+        products.add("Keyboard");
+        products.add("Monitor");
+        products.add("Printer");
 
-        car.startEngine();
-        refuelVehicle(car);
-        System.out.println(" ");
+        System.out.println("All products:");
+        printAll(products);
 
-        truck.startEngine();
-        refuelVehicle(truck);
+        products.add("Webcam");
+        boolean removed = products.remove("Mouse");
+        System.out.println("\nAfter adding and removing products:");
+        printAll(products);
 
-        System.out.println(" ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter product name to search: ");
+        String search = scanner.nextLine();
 
-        destroyVehicle(car);
-        destroyVehicle(truck);
-
-    }
-        private static void refuelVehicle(Vehicle refuelVehicle) {
-            refuelVehicle.refuel();
+        boolean found = false;
+        for (String product : products) {
+            if (product.equalsIgnoreCase(search)) {
+                System.out.println("Product found: " + product);
+                found = true;
+                break;
+            }
         }
-        private static void destroyVehicle(Vehicle destroyVehicle) {
-            destroyVehicle.destroy();
+
+        if (!found) {
+            System.out.println("Product not found.");
+        }
+        scanner.close();
+    }
+
+    private static void printAll(List<String> product) {
+
+        for (int i = 0; i < product.size(); i++) {
+            System.out.println((i + 1) + ". " + product.get(i));
+        }
+
     }
 }
