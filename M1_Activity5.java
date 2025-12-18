16a672cd1b5420df1888a7404af550ba3040f9ad
Charls Francis M. Iglesia <cfmiglesia@headoffice.corp.bpi.com.ph>
2025-12-18 15:58:22 +0800

    add M1_Activity5

---
+++ M1_Activity5.java
@@ -0,0 +1,17 @@
+package com.bpi.activity5.main;
+
+public class activity5 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        int sum = 0;
+
+        for (int i = 1; i <= 50; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+
+}
